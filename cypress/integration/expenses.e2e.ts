d3a8160c330abf5e05cd077cfb2e7f09857825bb
Jean-Michel Dagenais <jm@obkio.com>
2022-05-04 10:31:50 -0400

    module 4 assignment

--- cypress/integration/expenses.e2e.ts
+++ cypress/integration/expenses.e2e.ts
@@ -14,9 +14,11 @@ describe("Expenses page", () => {
     cy.contains("Value");
   });
 
-  it.skip("adds a new expense", () => {
-    const date = new Date().toLocaleDateString().replace(/\./g, "/");
-    const expectedDate = date.substring(0, 6) + date.substr(8);
+  it("adds a new expense", () => {
+    // const date = new Date().toLocaleDateString().replace(/\./g, "/");
+    // const expectedDate = date.substring(0, 6) + date.substr(8);
+    const date = '5/1/22';
+    const expectedDate = '01/05/22';
     const cp = getRandomString();
     const value = getRandomNumber();
 
@@ -30,8 +32,10 @@ describe("Expenses page", () => {
     cy.get("mat-option").contains("Shopping").click();
     cy.get("[data-cy=expense-value]").clear().type(value.toString());
 
-    // click save button
-    cy.contains("Save").click();
+    // click save
+    // debugger;
+    cy.get("[data-cy=save-btn]").click();
+    // cy.contains("Save").click();
 
     // expect success message
     cy.contains("Success");
